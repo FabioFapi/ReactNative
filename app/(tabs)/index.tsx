0163ffa2c1041dbfae7d615587ebbd0090184e28
FabioFapi <fabio.recchia@edu.itspiemonte.it>
2024-10-01 16:16:16 +0200

    correzioneEs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,123 +1,37 @@
-import { Text, View, FlatList, ListRenderItem, TextInput} from "react-native";
-import { Card, cards } from "@/api/data.mock";
-import { CardComponent } from "@/components/molecules/cardComponent/cardComponent.molecule";
-import { useState } from "react";
-import { ButtonComponent } from "@/components/atoms/button/button.atom";
-/*
+import { Text, View } from 'react-native';
+import { ButtonComponent } from '@/components/atoms/button/button.atom';
+import { useState } from 'react';
+import { TextInputComponent } from '@/components/atoms/textInput/textInput.atom';
+
 export default function Index() {
-  const[counter, setCounter] = useState(0);
-  const [resetCounter, setResetCounter] = useState(0);
+  const [text, setText] = useState('');
+  const [isConfirmPressed, setIsConfirmPressed] = useState(false);
 
-  const onPress = (): void => {
-    setCounter((prevState) => (prevState + 1));
-    setResetCounter(0);
+  // ** CALLBACKS  ** //
+  const onChangeText = (text: string) => setText(text);
+  const onPress = () => {
+    setIsConfirmPressed(true);
   };
 
-  const resetOnPress = (): void => {
-    setCounter((prevState) => (prevState = 0));
-    setResetCounter(1);
-  };
-*/
-export default function App() {
-  const [inputText, setInputText] = useState('');
-  const [displayText, setDisplayText] = useState('');
-/*
-  const handleInputChange = (e) => {
-    setInputText(e.target.value);
-  };
-*/
-  const handleShowText = () => {
-    setDisplayText(inputText);
+  const onReset = () => {
+    setText('');
+    setIsConfirmPressed(false);
   };
 
-  const handleReset = () => {
-    setInputText('');
-    setDisplayText('');
-  };
-
-
-const renderItem: ListRenderItem<Card> = ({ item, index }) => {
+  // ** UI  ** //
   return (
-    <CardComponent
-      key={index}
-      title={item.title}
-      subTitle={item.subTitle}
-      backgroundColor={item.backgroundColor}
-      image={item.image}
-    />
-  );
-};
-const ItemSeparatorComponent = () => <View style={{ height: 16 }} />;
-const ListHeaderComponent = () => {
-  return (
-    <Text style={{ fontSize: 24, paddingVertical: 16, textAlign: "center" }}>
-      Le card di oggi:{" "}
-    </Text>
-  );
-};
-const ListFooterComponent = () => {
-  return (
-    <Text style={{ fontSize: 24, paddingVertical: 16, textAlign: "center" }}>
-      Fine della lista
-    </Text>
-  );
-};
-
-const ListEmptyComponent = () => {
-  return (
-    <Text style={{ fontSize: 24, paddingVertical: 16, textAlign: "center" }}>
-      Nessuna card da mostrare
-    </Text>
-  );
-};
-/*
-export default function Index() {
-  return (
-    <FlatList
-      style={{ flex: 1 }}
-      bounces={false}
-      data={cards}
-      renderItem={renderItem}
-      ItemSeparatorComponent={ItemSeparatorComponent}
-      ListHeaderComponent={ListHeaderComponent}
-      ListFooterComponent={ListFooterComponent}
-      ListEmptyComponent={ListEmptyComponent}
-    />
-  );
-}
-
-return (
-  <View style={{ flex: 1 , justifyContent: "center"}}>
-    <Text style={{fontSize: 18, paddingVertical: 32, textAlign: "center"}}>Counter:{counter}</Text>
-    <Text style={{fontSize: 18, paddingVertical: 32, textAlign: "center"}}>Fottiti { resetCounter == 1 ? " " : "non"}</Text>
-    <ButtonComponent title="Finocchio" onPress={onPress} disabled={false}></ButtonComponent>
-    <ButtonComponent title="Non Finocchio" onPress={resetOnPress} disabled={counter <= 9 ? true : false}></ButtonComponent>
-  </View>
-)
-  */
-
-  return (
-    <View>
-      <h1 >Text Display App</h1>
-      <SafeAreaView>
-        <TextInput
-          placeholder="BANANE DA PER TUTTOOO"
-          value={inputText}
-          onChange={ (e) => {
-            setInputText(e.target.value)}}
-        />
-        <View>
-          <ButtonComponent onClick={handleShowText}>
-            Mostra Testo
-          </ButtonComponent>
-          <ButtonComponent onClick={handleReset} >
-            Reset
-          </ButtonComponent>
-        </View>
-        <Text style={{fontSize: 18, paddingVertical: 32, textAlign: "center"}}>
-          Testo visualizzato:{displayText}
-        </Text>
-      </SafeAreaView>
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <TextInputComponent value={text} onChangeText={onChangeText} />
+      {isConfirmPressed ? (
+        <Text style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}>{text}</Text>
+      ) : null}
+      <Text style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}></Text>
+      <ButtonComponent title="Cliccami" onPress={onPress} />
+      <ButtonComponent
+        style={{ marginTop: 16, backgroundColor: 'red' }}
+        title="Reset"
+        onPress={onReset}
+      />
     </View>
   );
 }
