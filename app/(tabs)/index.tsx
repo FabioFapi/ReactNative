1e96d47db17963e7969885ec80184d09b2814178
FabioFapi <fabio.recchia@edu.itspiemonte.it>
2024-10-01 17:41:05 +0200

    Roba in più

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,37 +1,42 @@
-import { Text, View } from 'react-native';
-import { ButtonComponent } from '@/components/atoms/button/button.atom';
-import { useState } from 'react';
-import { TextInputComponent } from '@/components/atoms/textInput/textInput.atom';
+import { Text, View, FlatList, ListRenderItem } from "react-native";
+import { Card, cards } from "@/api/data.mock";
+import { CardComponent } from "@/components/molecules/cardComponent/cardComponent.molecule";
+import { useState } from "react";
+import { ButtonComponent } from "@/components/atoms/button/button.atom";
 
 export default function Index() {
-  const [text, setText] = useState('');
-  const [isConfirmPressed, setIsConfirmPressed] = useState(false);
+  const[name, setName] = useState("Giorgia");
 
-  // ** CALLBACKS  ** //
-  const onChangeText = (text: string) => setText(text);
-  const onPress = () => {
-    setIsConfirmPressed(true);
+  const onPress = (): void => {
+    setCounter((prevState) => (prevState + 1));
   };
 
-  const onReset = () => {
-    setText('');
-    setIsConfirmPressed(false);
+  const onPress5 = (): void => {
+
+    for(var i; i< 5; i++){
+      onPress()
+    }
+
   };
 
-  // ** UI  ** //
-  return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <TextInputComponent value={text} onChangeText={onChangeText} />
-      {isConfirmPressed ? (
-        <Text style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}>{text}</Text>
-      ) : null}
-      <Text style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}></Text>
-      <ButtonComponent title="Cliccami" onPress={onPress} />
-      <ButtonComponent
-        style={{ marginTop: 16, backgroundColor: 'red' }}
-        title="Reset"
-        onPress={onReset}
-      />
-    </View>
-  );
+const onChangeName = () => {
+  setName("Fabio")
+};
+
+  const user = useMemo(() => {
+    return{
+    name: name,
+    surname:"Piazza",
+    age:20,
+  };
+}, [name]);
+
+
+return (
+  <View style={{ flex: 1 , justifyContent: "center"}}>
+    <Text style={{fontSize: 18, paddingVertical: 32, textAlign: "center"}}>{user.name}</Text>
+    <ButtonComponent title="+1" onPress={onPress} disabled={false}></ButtonComponent>
+    <ButtonComponent title="+5" onPress={onPress5} ></ButtonComponent>
+  </View>
+)
 }
