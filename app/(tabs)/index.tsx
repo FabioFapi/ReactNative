25aa6280ed13e17b7b15e77025224f109a61e8e8
FabioFapi <fabio.recchia@edu.itspiemonte.it>
2024-10-22 14:28:10 +0200

    22/10/2024 14:45

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,78 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import { ButtonComponent } from '@/components/atoms/button/button.atom';
-import { useMemo, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { TextInputComponent } from '@/components/atoms/textInput/textInput.atom';
+import { useMemo } from 'react';
+import { TextInput } from 'react-native-gesture-handler';
 
 export default function Index() {
-  const [name, setName] = useState('Mario');
+  const [quantity, setQuantity] = useState(1);
+  const [text, setText] = useState('');
+  const [shownText, setShownText] = useState('');
 
-  // ** CALLBACKS  ** //
-  const onChangeName = () => {
-    setName('Luigi');
+  useEffect(() => {
+    alert('Benvenuto');
+  }, []);
+
+  useEffect(() => {
+    setShownText(text);
+  }, [text]);
+
+  const onChangedText = (newText: string) => {
+    setText(newText);
   };
 
-  console.log('name', name);
-
-  const user = useMemo(() => {
+  //useMemo
+  const productPrice = useMemo(() => {
     return {
-      name: name,
-      surname: 'Rossi',
-      age: 25,
+      price: 30,
+      quantity: quantity,
+      discount: 10,
+      additionalPrice: 5,
     };
-  }, [name]);
+  }, [quantity]);
+
+  // ** CALLBACKS  ** //
+  const onPressAddOne = () => {
+    setQuantity((prevState) => prevState + 1);
+  };
+
+  const onPressRemoveOne = () => {
+    setQuantity((prevState) => (prevState == 1 ? 1 : prevState - 1));
+  };
+
+  const calculatePrice = useMemo(() => {
+    return {
+      totalPrice:
+        (productPrice.price - productPrice.discount) * quantity + productPrice.additionalPrice,
+    };
+  }, [quantity]);
+
+  const incrementQuantity = useCallback(onPressAddOne, []);
+  const decrementQuantity = useCallback(onPressRemoveOne, []);
 
   // ** UI  ** //
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Text style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}>{user.name}</Text>
-      <ButtonComponent title="Cliccami" onPress={onChangeName} />
+    <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
+      <Text style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}>
+        Price: ${productPrice.price}
+        {'\n'}
+        Discount: ${productPrice.discount}
+        {'\n'}
+        Quantity: {productPrice.quantity}
+        {'\n'}
+        Additional Price: ${productPrice.additionalPrice}
+        {'\n'}
+        Total: ${calculatePrice.totalPrice}
+      </Text>
+      <TextInput
+        style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}
+        value={text}
+        onChangeText={onChangedText}
+        placeholder="ciao"></TextInput>
+      <Text style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}>{shownText}</Text>
+      <ButtonComponent title="Add 1" onPress={incrementQuantity} />
+      <ButtonComponent style={{ margin: 5 }} title="Remove 1" onPress={decrementQuantity} />
     </View>
   );
 }
