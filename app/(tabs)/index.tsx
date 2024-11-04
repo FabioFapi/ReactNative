609e4e72ccbbfea6a5d667eed8daed115ef1e193
FabioFapi <fabio.recchia@edu.itspiemonte.it>
2024-11-04 15:47:13 +0100

    15:47 04/11/2024

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,78 +1,78 @@
-import { Button, Text, View } from 'react-native';
-import { ButtonComponent } from '@/components/atoms/button/button.atom';
-import { useCallback, useEffect, useState } from 'react';
-import { TextInputComponent } from '@/components/atoms/textInput/textInput.atom';
-import { useMemo } from 'react';
-import { TextInput } from 'react-native-gesture-handler';
+import React, { useCallback, useState } from 'react';
+import { RowItemMolecule } from '@/components/molecules/rowItem/rowItem.molecule';
+import { Button, FlatList, ListRenderItem, TextInput, View } from 'react-native';
+import { styles } from '@/app/styles';
+
+interface Task {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+}
 
 export default function Index() {
-  const [quantity, setQuantity] = useState(1);
-  const [text, setText] = useState('');
-  const [shownText, setShownText] = useState('');
-
-  useEffect(() => {
-    alert('Benvenuto');
-  }, []);
-
-  useEffect(() => {
-    setShownText(text);
-  }, [text]);
-
-  const onChangedText = (newText: string) => {
-    setText(newText);
-  };
-
-  //useMemo
-  const productPrice = useMemo(() => {
-    return {
-      price: 30,
-      quantity: quantity,
-      discount: 10,
-      additionalPrice: 5,
-    };
-  }, [quantity]);
+  const [userTask, setUserTask] = useState('');
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [isTextInputFullFilled, setIsTextInputFullFilled] = useState(false);
 
   // ** CALLBACKS  ** //
-  const onPressAddOne = () => {
-    setQuantity((prevState) => prevState + 1);
-  };
+  const addTask = useCallback(() => {
+    if (!userTask.length) {
+      return;
+    }
 
-  const onPressRemoveOne = () => {
-    setQuantity((prevState) => (prevState == 1 ? 1 : prevState - 1));
-  };
+    setTasks((prevState) => {
+      return [
+        ...prevState,
+        {
+          id: new Date().valueOf().toString(),
+          title: userTask,
+          isCompleted: false,
+        },
+      ];
+    });
+    setUserTask('');
+    setIsTextInputFullFilled(true);
+  }, [userTask]);
 
-  const calculatePrice = useMemo(() => {
-    return {
-      totalPrice:
-        (productPrice.price - productPrice.discount) * quantity + productPrice.additionalPrice,
-    };
-  }, [quantity]);
-
-  const incrementQuantity = useCallback(onPressAddOne, []);
-  const decrementQuantity = useCallback(onPressRemoveOne, []);
+  const manageTask = useCallback((id: string) => {
+    setTasks((prevState) => {
+      return prevState.map((task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            isCompleted: true,
+          };
+        }
+        return task;
+      });
+    });
+  }, []);
 
   // ** UI  ** //
+  const renderItem: ListRenderItem<Task> = useCallback(
+    ({ item }) => {
+      return (
+        <RowItemMolecule
+          title={item.title}
+          isCompleted={item.isCompleted}
+          onPress={() => manageTask(item.id)}
+        />
+      );
+    },
+    [manageTask]
+  );
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
-      <Text style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}>
-        Price: ${productPrice.price}
-        {'\n'}
-        Discount: ${productPrice.discount}
-        {'\n'}
-        Quantity: {productPrice.quantity}
-        {'\n'}
-        Additional Price: ${productPrice.additionalPrice}
-        {'\n'}
-        Total: ${calculatePrice.totalPrice}
-      </Text>
-      <TextInput
-        style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}
-        value={text}
-        onChangeText={onChangedText}
-        placeholder="ciao"></TextInput>
-      <Text style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}>{shownText}</Text>
-      <ButtonComponent title="Add 1" onPress={incrementQuantity} />
-      <ButtonComponent style={{ margin: 5 }} title="Remove 1" onPress={decrementQuantity} />
+    <View style={styles.container}>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          style={isTextInputFullFilled ? styles.textInput : styles.emptyTextInput}
+          value={userTask}
+          onChangeText={setUserTask}
+        />
+        <Button title={'Add'} onPress={addTask} />
+      </View>
+      <FlatList data={tasks} renderItem={renderItem} />
     </View>
   );
 }
